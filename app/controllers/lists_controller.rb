2b0fac8542f762ce11780e94495073782822d215
kikuishimoto <kikuish@gmail.com>
2020-09-21 15:13:39 -0700

    added params

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,13 @@
 class ListsController < ApplicationController
     def index
         lists = List.all
-        render json: ListSerializer.new(lists)
+        render json: ListSerializer.new(lists, {include: [:phrases]})
     end 
 
     def create
         list = List.new(list_params)
         if list.save
-            render json: ListSerializer.new(list)
+            render json: ListSerializer(list)
         else 
             render json: {message: "list can not be created"}
         end 
@@ -17,6 +17,12 @@ class ListsController < ApplicationController
         list = List.find(params[:id])
         list.destroy 
         render json: {message: "#{list.name} has been deleted"}
+    end 
+
+    private 
+
+    def list_params
+        params.require(:dog).permit(:name)
     end 
 
 end

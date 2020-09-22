f50e0d6cb643ff76841b39177364b6d3f02f2e6e
kikuishimoto <kikuish@gmail.com>
2020-09-22 12:35:41 -0700

    updated raltionships and added attributes

--- app/serializers/phrase_serializer.rb
+++ app/serializers/phrase_serializer.rb
@@ -1,6 +1,6 @@
 class PhraseSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :content, :image, :pinyin
-    belongs_to :list
+    attributes :content, :image, :pinyin, :lists
+    belongs_to :category
     
 end 

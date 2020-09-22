c562684eeb182662d3e6aa3a12ffc9a5958b93a6
kikuishimoto <kikuish@gmail.com>
2020-09-22 12:36:30 -0700

    added join tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_183352) do
+ActiveRecord::Schema.define(version: 2020_09_22_185635) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -22,10 +28,30 @@ ActiveRecord::Schema.define(version: 2020_09_21_183352) do
     t.string "content"
     t.string "image"
     t.string "pinyin"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_phrases_on_category_id"
+  end
+
+  create_table "phrases_lists", force: :cascade do |t|
+    t.integer "phrase_id"
     t.integer "list_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["list_id"], name: "index_phrases_on_list_id"
+    t.index ["list_id"], name: "index_phrases_lists_on_list_id"
+    t.index ["phrase_id"], name: "index_phrases_lists_on_phrase_id"
   end
 
+  create_table "tutors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "image"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "phrases_lists", "lists"
+  add_foreign_key "phrases_lists", "phrases"
 end

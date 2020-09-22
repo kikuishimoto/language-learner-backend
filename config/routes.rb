3ef35984c43688c92d76c25118eeea8a151d1019
kikuishimoto <kikuish@gmail.com>
2020-09-22 12:36:21 -0700

    create tutor and category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :lists
+  resources :tutors
+  resources :categories
   resources :phrases
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
